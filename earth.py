64be63548addd2b62c17c7b91089445ca6799eb5
MateoLeonov <mjleonov@mail.austral.edu.ar>
2025-03-31 14:27:08 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,4 +1,8 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
-    
+
+    z0 = x < y
+    z = x > y
+    print(f"The result of {x} comes first in the dictionary than {y} is {z0}")
+    print(f"The result of {x} comes first in the dictionary than {y} is {z}")
