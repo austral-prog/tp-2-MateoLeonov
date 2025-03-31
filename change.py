2d99c3a4bb0ef5b5d5079156862798d4359d4613
MateoLeonov <mjleonov@mail.austral.edu.ar>
2025-03-31 14:33:40 -0300

    Update change.py

--- change.py
+++ change.py
@@ -2,11 +2,11 @@ def change():
     expense = 23.75
     money = 100
 
-print(f"Ingresar gasto: \n{expense}")
-print(f"Dinero recibido \n{money}\n")
-print(f"Vuelto \n\nPesos:")
-total_int = int(money) - int(expense) - 1
-print(total_int)
-print(f"Centavos:")
-resto_float = int((money - expense - total_int) *100)
-print(resto_float)
+    print(f"Ingresar gasto: \n{expense}")
+    print(f"Dinero recibido \n{money}\n")
+    print(f"Vuelto \n\nPesos:")
+    total_int = int(money) - int(expense) - 1
+    print(total_int)
+    print(f"Centavos:")
+    resto_float = int((money - expense - total_int) *100)
+    print(resto_float)
